58426624ee56d3eac7f7bdef47fc9d755de3b153
vikas <khanchandani58@gmail.com>
2022-12-19 18:41:10 +0000

    vercel2

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use((error, req, res, next) => {
-  if (res.headersSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({
-    message: error.message || "An unknown error has occured",
-  });
-});
+// app.use((error, req, res, next) => {
+//   if (res.headersSent) {
+//     return next(error);
+//   }
+//   res.status(error.code || 500);
+//   res.json({
+//     message: error.message || "An unknown error has occured",
+//   });
+// });
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
