898f9c34900907eae1e280ccd8332a982a86fe72
vikas <khanchandani58@gmail.com>
2022-12-19 18:21:30 +0000

    cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api/client", rutasClient);
 app.use("/api/invoice", rutasInvoice);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
